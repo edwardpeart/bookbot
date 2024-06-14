2b6437cf7e64456358413a1b3d9a955a32c3cdd2
edwardpeart <edward.peart@hotmail.com>
2024-06-14 14:38:41 +0100

    Added character counter report

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     lowered_chars = text.lower()
-    num_chars = get_num_chars(lowered_chars)
+
+    char_dict = get_num_chars(lowered_chars)
+    sorted_chars = sort_char_counts(char_dict)
+    
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(f"Number of each letter in the document {num_chars}")
+    for char, count in sorted_chars:
+        print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -17,12 +23,18 @@ def get_book_text(book_path):
     
 def get_num_chars(lowered_chars):
     char_dict = {}
-    for char in set(lowered_chars):
+    for char in lowered_chars:
         if char.isalpha():
-            char_dict[char] = lowered_chars.count(char)
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
     return char_dict
+
+def sort_char_counts(char_dict):
+    sorted_list = sorted(char_dict.items(), key=lambda item: item[1], reverse=True)
+    return sorted_list
+
 
 if __name__ == "__main__":
     main()
-
-   
